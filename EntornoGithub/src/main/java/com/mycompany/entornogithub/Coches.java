32b2b9270af9eab470aba1d100d7388806b38b2b
Pablo Miranda <mirandahidalgopablo@gmail.com>
2026-06-02 09:44:35 +0200

    Creacion del codigo
    Se añade el codigo de Netbeans al repositorio

---
+++ EntornoGithub/src/main/java/com/mycompany/entornogithub/Coches.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.entornogithub;
+
+public class Coches {
+    private String marca;
+    private double km;
+    private int año;
+    private int cv;
+    private String motor;
+    private String nombre;
+    private int capacidad;
+}
